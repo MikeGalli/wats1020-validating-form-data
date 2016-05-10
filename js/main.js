2088186d9cc72cf77abd3aee948f70c7c17219b2
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-05-10 16:44:27 -0700

    k

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).on('ready', function(){
+  $('#order-form').validate({
+      submitHandler: function(form) {
+          // If form is valid, submit it!
+          form.submit();
+      },
+      rules: {
+          "your-name": {
+              required: true,
+              maxlength: 128
+          },
+
+
+
+
+        }
+});
